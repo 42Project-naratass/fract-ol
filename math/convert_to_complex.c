bb516a0e7930a0891261f2aa1f0b80cc8e4dd996
KILLVader8550 <killvader8550@gmail.com>
2026-05-21 17:12:47 +0700

    optimization: merge offset&&convert_to+complex function to reduce funtion call

--- math/convert_to_complex.c
+++ math/convert_to_complex.c
@@ -4,7 +4,8 @@ t_complex	convert_to_complex(t_data *img, const int p_x, const int p_y)
 {
     t_complex	complex;
 
-    complex = offset_cal(p_x, p_y);
+    complex.real = X_MIN + (p_x / WIDTH) * (X_MAX - X_MIN);
+    complex.i = Y_MAX - (p_y / HEIGHT) * (Y_MAX- Y_MIN);
     complex.real = (img->m_x - complex.real) / img->zoom;
     complex.i = (img->m_y - complex.i) / img->zoom;
     return (complex);
