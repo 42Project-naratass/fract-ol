3b39ae2071b5c52c77ee73bd5bc534b0edd5e694
KILLVader8550 <killvader8550@gmail.com>
2026-05-21 21:28:47 +0700

    optimization: change calc_mandel algorithm

--- mandelbort/calc_mandelbrot.c
+++ mandelbort/calc_mandelbrot.c
@@ -2,25 +2,30 @@
 
 int	calc_mandelbrot(t_data *img, const int p_x, const int p_y) 
 {
-	double	x;
-	double	y;
-	int	iter;
-	double	x_temp;
+	double		x;
+	double		y;
+	int		iter;
 	t_complex	complex;
+	double		xsqr;
+	double		ysqr;
 
 	x = 0;
 	y = 0;
+	xsqr = x;
+	ysqr = y;
 	iter = 0;
 	complex.real = X_MIN + (p_x / WIDTH) * (X_MAX - X_MIN);
 	complex.i = Y_MAX - (p_y / HEIGHT) * (Y_MAX- Y_MIN);
 	complex.real = (img->m_x - complex.real) / img->zoom;
 	complex.i = (img->m_y - complex.i) / img->zoom;
-	while ((x * x) + (y * y) <= 4 && iter < MAX_ITER) // escape time algorithm
+	while (xsqr + ysqr <= 4 && iter < MAX_ITER) // escape time algorithm
 	{
-		x_temp = (x * x) - (y * y) + complex.real + img->x_offset;
-		y = 2 * x * y + complex.i + img->y_offset;
-		x = x_temp;
-		++iter;
+	    y = (x + y) * (x + y) - xsqr - ysqr;
+	    y += complex.i + img->y_offset;
+	    x = xsqr - ysqr + complex.real + img->x_offset;
+	    xsqr = x * x;
+	    ysqr = y * y;
+	    ++iter;
 	}
 	return (iter);
 }
