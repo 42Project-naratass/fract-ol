318e382584381f74d40ded533de3de431a644ce1
KILLVader8550 <killvader8550@gmail.com>
2026-05-26 02:20:27 +0700

    Delete: line

--- event/ft_close_win.c
+++ event/ft_close_win.c
@@ -6,7 +6,7 @@
 /*   By: naratass <naratass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 01:47:36 by naratass          #+#    #+#             */
-/*   Updated: 2026/05/26 01:47:37 by naratass         ###   ########.fr       */
+/*   Updated: 2026/05/26 02:20:10 by naratass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	ft_close_win(t_data *data)
 {
-	ft_printf("End program\n");
 	mlx_destroy_image(data->mlx, data->img);
 	mlx_destroy_window(data->mlx, data->mlx_win);
 	free(data->mlx);
